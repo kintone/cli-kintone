b2e46c9967b58eb9cad73539011ebe3bbc29f87c
Masaharu TASHIRO <masatsr.kit@gmail.com>
2022-08-16 21:58:15 +0900

    test: fix tests

---
+++ src/record/export/printers/printAsCsv/__tests__/fixtures/index/common/layout.ts
@@ -0,0 +1,94 @@
+import { LayoutJson } from "../../../../../../../../kintone/types";
+
+export const layoutJson: LayoutJson = {
+  revision: "29",
+  layout: [
+    {
+      type: "ROW",
+      fields: [
+        { type: "RECORD_NUMBER", code: "recordNumber", size: { width: "123" } },
+        { type: "UPDATED_TIME", code: "updatedTime", size: { width: "123" } },
+        {
+          type: "DROP_DOWN",
+          code: "dropDown",
+          size: { width: "123" },
+        },
+        { type: "CREATOR", code: "creator", size: { width: "123" } },
+        { type: "MODIFIER", code: "modifier", size: { width: "123" } },
+        {
+          type: "RICH_TEXT",
+          code: "richText",
+          size: { width: "123", innerHeight: "123" },
+        },
+      ],
+    },
+    {
+      type: "ROW",
+      fields: [
+        {
+          type: "SINGLE_LINE_TEXT",
+          code: "singleLineText",
+          size: { width: "123" },
+        },
+        { type: "NUMBER", code: "number", size: { width: "123" } },
+        { type: "RADIO_BUTTON", code: "radioButton", size: { width: "123" } },
+        {
+          type: "MULTI_LINE_TEXT",
+          code: "multiLineText",
+          size: { width: "123", innerHeight: "123" },
+        },
+        { type: "CREATED_TIME", code: "createdTime", size: { width: "123" } },
+        { type: "CHECK_BOX", code: "checkBox", size: { width: "123" } },
+      ],
+    },
+    {
+      type: "ROW",
+      fields: [
+        {
+          type: "CALC",
+          code: "calc",
+          size: { width: "123" },
+        },
+        {
+          type: "MULTI_LINE_TEXT",
+          code: "multiSelect",
+          size: { width: "123", innerHeight: "123" },
+        },
+      ],
+    },
+    {
+      type: "GROUP",
+      code: "group",
+      layout: [
+        {
+          type: "ROW",
+          fields: [
+            { type: "USER_SELECT", code: "userSelect", size: { width: "123" } },
+            {
+              type: "ORGANIZATION_SELECT",
+              code: "organizationSelect",
+              size: { width: "123" },
+            },
+            {
+              type: "GROUP_SELECT",
+              code: "groupSelect",
+              size: { width: "123" },
+            },
+          ],
+        },
+        {
+          type: "ROW",
+          fields: [
+            { type: "DATE", code: "date", size: { width: "123" } },
+            {
+              type: "DATETIME",
+              code: "dateTime",
+              size: { width: "123" },
+            },
+            { type: "TIME", code: "time", size: { width: "123" } },
+          ],
+        },
+      ],
+    },
+  ],
+};
--- src/record/export/printers/printAsCsv/__tests__/header.test.ts
+++ src/record/export/printers/printAsCsv/__tests__/header.test.ts
@@ -1,22 +1,54 @@
-import type { FieldsJson } from "../../../../../kintone/types";
+import type { FieldsJson, LayoutJson } from "../../../../../kintone/types";
 
 import { buildHeaderFields } from "../header";
 import { PRIMARY_MARK } from "../constants";
+import { defaultStrategy } from "../strategies/defaultStrategy";
 
 const fieldsJson: FieldsJson = require("./fixtures/header/fields.json");
+const layoutJson: LayoutJson = require("./fixtures/header/layout.json");
 const fileFieldsJson: FieldsJson = require("./fixtures/header/file_fields.json");
+const fileLayoutJson: LayoutJson = require("./fixtures/header/file_layout.json");
 const subtableFieldsJson: FieldsJson = require("./fixtures/header/subtable_fields.json");
+const subtableLayoutJson: LayoutJson = require("./fixtures/header/subtable_layout.json");
 
 describe("buildHeaderFields", () => {
   it("should generate fieldCode array correctly (data without subtable)", () => {
-    expect(buildHeaderFields(fieldsJson).includes(PRIMARY_MARK)).toBe(false);
-    expect(buildHeaderFields(fieldsJson)).toHaveLength(20);
-    expect(buildHeaderFields(fileFieldsJson)).toHaveLength(15);
+    expect(
+      buildHeaderFields(
+        fieldsJson,
+        layoutJson,
+        defaultStrategy(fieldsJson, layoutJson)
+      ).includes(PRIMARY_MARK)
+    ).toBe(false);
+    expect(
+      buildHeaderFields(
+        fieldsJson,
+        layoutJson,
+        defaultStrategy(fieldsJson, layoutJson)
+      )
+    ).toHaveLength(20);
+    expect(
+      buildHeaderFields(
+        fileFieldsJson,
+        fileLayoutJson,
+        defaultStrategy(fileFieldsJson, fileLayoutJson)
+      )
+    ).toHaveLength(15);
   });
   it("should generate fieldCode array correctly (data with subtable)", () => {
-    expect(buildHeaderFields(subtableFieldsJson).includes(PRIMARY_MARK)).toBe(
-      true
-    );
-    expect(buildHeaderFields(subtableFieldsJson)).toHaveLength(22);
+    expect(
+      buildHeaderFields(
+        subtableFieldsJson,
+        subtableLayoutJson,
+        defaultStrategy(subtableFieldsJson, subtableLayoutJson)
+      ).includes(PRIMARY_MARK)
+    ).toBe(true);
+    expect(
+      buildHeaderFields(
+        subtableFieldsJson,
+        subtableLayoutJson,
+        defaultStrategy(subtableFieldsJson, subtableLayoutJson)
+      )
+    ).toHaveLength(22);
   });
 });

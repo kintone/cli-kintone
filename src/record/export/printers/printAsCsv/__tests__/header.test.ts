57bceea5d26ea34ce6ee2b764d6f00bf180ac3f6
Masaharu TASHIRO <masatsr.kit@gmail.com>
2022-08-17 16:13:23 +0900

    test: fix tests

--- src/record/export/printers/printAsCsv/__tests__/header.test.ts
+++ src/record/export/printers/printAsCsv/__tests__/header.test.ts
@@ -7,6 +7,8 @@ const fieldsJson: FieldsJson = require("./fixtures/header/fields.json");
 const layoutJson: LayoutJson = require("./fixtures/header/layout.json");
 const fileFieldsJson: FieldsJson = require("./fixtures/header/file_fields.json");
 const fileLayoutJson: LayoutJson = require("./fixtures/header/file_layout.json");
+const systemFieldsFieldsJson: FieldsJson = require("./fixtures/header/systemfields_fields.json");
+const systemFieldsLayoutJson: LayoutJson = require("./fixtures/header/systemfields_layout.json");
 const subtableFieldsJson: FieldsJson = require("./fixtures/header/subtable_fields.json");
 const subtableLayoutJson: LayoutJson = require("./fixtures/header/subtable_layout.json");
 
@@ -15,8 +17,70 @@ describe("buildHeaderFields", () => {
     expect(
       buildHeaderFields(fieldsJson, layoutJson).includes(PRIMARY_MARK)
     ).toBe(false);
-    expect(buildHeaderFields(fieldsJson, layoutJson)).toHaveLength(20);
-    expect(buildHeaderFields(fileFieldsJson, fileLayoutJson)).toHaveLength(15);
+
+    expect(buildHeaderFields(fieldsJson, layoutJson)).toStrictEqual([
+      "recordNumber",
+      "updatedTime",
+      "dropDown",
+      "creator",
+      "modifier",
+      "richText",
+      "singleLineText",
+      "number",
+      "radioButton",
+      "multiLineText",
+      "createdTime",
+      "checkBox",
+      "calc",
+      "multiSelect",
+      "userSelect",
+      "organizationSelect",
+      "groupSelect",
+      "date",
+      "dateTime",
+      "time",
+    ]);
+    expect(buildHeaderFields(fileFieldsJson, fileLayoutJson)).toStrictEqual([
+      "recordNumber",
+      "updatedTime",
+      "dropDown",
+      "creator",
+      "modifier",
+      "richText",
+      "singleLineText",
+      "number",
+      "radioButton",
+      "multiLineText",
+      "createdTime",
+      "checkBox",
+      "calc",
+      "multiSelect",
+      "file",
+    ]);
+    expect(
+      buildHeaderFields(systemFieldsFieldsJson, systemFieldsLayoutJson)
+    ).toStrictEqual([
+      "recordNumber",
+      "dropDown",
+      "richText",
+      "singleLineText",
+      "number",
+      "radioButton",
+      "multiLineText",
+      "checkBox",
+      "calc",
+      "multiSelect",
+      "userSelect",
+      "organizationSelect",
+      "groupSelect",
+      "date",
+      "dateTime",
+      "time",
+      "creator",
+      "createdTime",
+      "modifier",
+      "updatedTime",
+    ]);
   });
   it("should generate fieldCode array correctly (data with subtable)", () => {
     expect(
@@ -26,6 +90,29 @@ describe("buildHeaderFields", () => {
     ).toBe(true);
     expect(
       buildHeaderFields(subtableFieldsJson, subtableLayoutJson)
-    ).toHaveLength(22);
+    ).toStrictEqual([
+      "*",
+      "recordNumber",
+      "updatedTime",
+      "dropDown",
+      "creator",
+      "subTable",
+      "subTableText",
+      "subTableCheckbox",
+      "subTableFile",
+      "userSelect",
+      "organizationSelect",
+      "groupSelect",
+      "modifier",
+      "richText",
+      "singleLineText",
+      "number",
+      "radioButton",
+      "multiLineText",
+      "createdTime",
+      "checkBox",
+      "calc",
+      "multiSelect",
+    ]);
   });
 });

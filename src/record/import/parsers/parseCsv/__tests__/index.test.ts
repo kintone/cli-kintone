38d863f40f012c5ee85b38d3fe16412267ead2eb
Masaharu TASHIRO <masatsr.kit@gmail.com>
2022-08-31 15:00:25 +0900

    fix: unexpected error when the input is empty csv

--- src/record/import/parsers/parseCsv/__tests__/index.test.ts
+++ src/record/import/parsers/parseCsv/__tests__/index.test.ts
@@ -5,6 +5,7 @@ import { parseCsv } from "../index";
 
 import { pattern as withoutSubtable } from "./fixtures/withoutSubtable";
 import { pattern as withSubtable } from "./fixtures/withSubtable";
+import { pattern as emptyCsv } from "./fixtures/emptyCsv";
 
 export type TestPattern = {
   description: string;
@@ -14,7 +15,10 @@ export type TestPattern = {
 };
 
 describe("parseCsv", () => {
-  it.each([withoutSubtable, withSubtable])("$description", (pattern) => {
-    expect(parseCsv(pattern.input, pattern.schema)).toEqual(pattern.expected);
-  });
+  it.each([withoutSubtable, withSubtable, emptyCsv])(
+    "$description",
+    (pattern) => {
+      expect(parseCsv(pattern.input, pattern.schema)).toEqual(pattern.expected);
+    }
+  );
 });

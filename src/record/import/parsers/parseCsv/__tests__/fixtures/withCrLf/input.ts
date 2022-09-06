e2edc44503b54e23ec7928ed119f287d333bf00f
tasshi / Masaharu TASHIRO <33759872+mshrtsr@users.noreply.github.com>
2022-09-06 15:38:11 +0900

    feat: auto-discover line break represents record delimiter (#69)

---
+++ src/record/import/parsers/parseCsv/__tests__/fixtures/withCrLf/input.ts
@@ -0,0 +1,1 @@
+export const csv = `"singleLineText","multiLineText","multiSelect"\r\n"""single line text""","multi\nline\ntext","""sample3""\nsample4,sample5"\r\n"""single line text""","multi\nline\ntext","""sample4""\nsample5,sample6"\r\n`;

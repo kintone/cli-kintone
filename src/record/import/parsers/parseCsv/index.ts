dd6a23d3b2913ddb311cbb21ae203c2774f09c60
Masaharu TASHIRO <masatsr.kit@gmail.com>
2022-09-22 13:12:50 +0900

    fix: create ParserError

--- src/record/import/parsers/parseCsv/index.ts
+++ src/record/import/parsers/parseCsv/index.ts
@@ -6,23 +6,28 @@ import csvParse from "csv-parse/lib/sync";
 
 import { convertRecord, recordReader } from "./record";
 import { SEPARATOR } from "./constants";
+import { ParserError } from "../error";
 
 export const parseCsv: (
   csv: string,
   schema: RecordSchema
 ) => KintoneRecord[] = (csv, schema) => {
-  const rows: CsvRow[] = csvParse(csv, {
-    columns: true,
-    skip_empty_lines: true,
-    delimiter: SEPARATOR,
-  });
+  try {
+    const rows: CsvRow[] = csvParse(csv, {
+      columns: true,
+      skip_empty_lines: true,
+      delimiter: SEPARATOR,
+    });
 
-  const records: KintoneRecord[] = [];
+    const records: KintoneRecord[] = [];
 
-  for (const recordRows of recordReader(rows)) {
-    const record = convertRecord(recordRows, schema);
-    records.push(record);
+    for (const recordRows of recordReader(rows)) {
+      const record = convertRecord(recordRows, schema);
+      records.push(record);
+    }
+
+    return records;
+  } catch (e) {
+    throw new ParserError(e);
   }
-
-  return records;
 };
--- src/record/import/utils/log.ts
+++ src/record/import/utils/log.ts
@@ -1,5 +1,6 @@
 import { AddRecordsError } from "../usecases/add/error";
 import { UpsertRecordsError } from "../usecases/upsert/error";
+import { ParserError } from "../parsers/error";
 
 const logger = console.error;
 
@@ -8,6 +9,8 @@ export const printError = (error: unknown): void => {
     if (error instanceof AddRecordsError) {
       logger(error.toString());
     } else if (error instanceof UpsertRecordsError) {
+      logger(error.toString());
+    } else if (error instanceof ParserError) {
       logger(error.toString());
     } else {
       logger(error);

48f7a4ea07fc4bebbc0dddc119f3628ff2ed929b
Nguyen Thai Hung <hung-nguyen@cybozu.vn>
2022-11-14 13:59:54 +0700

    feat: providing subcommand for command completion

--- src/cli/main.ts
+++ src/cli/main.ts
@@ -2,4 +2,4 @@ import yargs from "yargs";
 import { recordCommand } from "./record";
 
 // eslint-disable-next-line no-unused-expressions
-yargs.command(recordCommand).demandCommand().strict().help().argv;
+yargs.completion('completion').command(recordCommand).demandCommand().strict().help().argv;

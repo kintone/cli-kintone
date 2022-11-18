ec8036e1c24cb0375fea86da7552f503d3918dfd
Nguyen Thai Hung <hung-nguyen@cybozu.vn>
2022-11-18 10:08:19 +0700

    feat: add a new commad - delete all records

--- src/cli/main.ts
+++ src/cli/main.ts
@@ -1,10 +1,12 @@
 import yargs from "yargs";
 import { recordCommand } from "./record";
+import { deleteCommand } from "./delete";
 
 // eslint-disable-next-line no-unused-expressions
 yargs
   .completion("completion")
   .command(recordCommand)
+  .command(deleteCommand)
   .demandCommand()
   .strict()
   .help().argv;

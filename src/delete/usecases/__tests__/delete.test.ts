69f8ecff140520ee08bef7ef331a3cf3a1210e3f
Nguyen Thai Hung <hung-nguyen@cybozu.vn>
2022-11-18 10:08:39 +0700

    test: add new test cases for deleting all records command

---
+++ src/delete/usecases/__tests__/delete.test.ts
@@ -0,0 +1,60 @@
+import { KintoneRestAPIClient } from "@kintone/rest-api-client";
+import { deleteAllRecords } from "../delete";
+
+describe("deleteAllRecords", () => {
+  let apiClient: KintoneRestAPIClient;
+  beforeEach(() => {
+    apiClient = new KintoneRestAPIClient({
+      baseUrl: "https://localhost/",
+      auth: { apiToken: "dummy" },
+    });
+  });
+
+  it("should not fail", () => {
+    apiClient.record.getAllRecordsWithId = jest.fn().mockResolvedValue([
+      {
+        $id: { value: "1" },
+        value1: {
+          values: "",
+        },
+      },
+    ]);
+    apiClient.record.deleteAllRecords = jest.fn().mockResolvedValue([{}]);
+    return expect(deleteAllRecords(apiClient, "1")).resolves.not.toThrow();
+  });
+
+  it("should pass parameters to the apiClient correctly", async () => {
+    apiClient.record.getAllRecordsWithId = jest.fn().mockResolvedValue([
+      {
+        $id: { value: "1" },
+        value1: {
+          values: "",
+        },
+      },
+    ]);
+    const deleteAllRecordsMockFn = jest.fn().mockResolvedValue([{}]);
+    apiClient.record.deleteAllRecords = deleteAllRecordsMockFn;
+    const APP_ID = "1";
+
+    await deleteAllRecords(apiClient, APP_ID);
+
+    expect(deleteAllRecordsMockFn.mock.calls[0][0]).toStrictEqual({
+      app: APP_ID,
+      records: [{ id: 1 }],
+    });
+  });
+
+  it("should throw error when API response is error", () => {
+    const error = new Error("Failed to delete all records.");
+    apiClient.record.getAllRecordsWithId = jest.fn().mockResolvedValue([
+      {
+        $id: { value: "1" },
+        value1: {
+          values: "",
+        },
+      },
+    ]);
+    apiClient.record.deleteAllRecords = jest.fn().mockRejectedValueOnce(error);
+    return expect(deleteAllRecords(apiClient, "1")).rejects.toThrow(error);
+  });
+});

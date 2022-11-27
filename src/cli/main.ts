6dd03cc1a9189ac5f09613301b603099e6061f87
Nguyen Thai Hung <hung-nguyen@cybozu.vn>
2022-11-27 21:55:07 +0700

    chore: push missing code

--- src/cli/main.ts
+++ src/cli/main.ts
@@ -1,10 +1,44 @@
 import yargs from "yargs";
 import { recordCommand } from "./record";
 
+// TODO: This is the Workaround of https://github.com/DefinitelyTyped/DefinitelyTyped/issues/63396
+// After the issue is fixed, we can remove this.
+type CompletionCallback = (
+  err: Error | null,
+  completions: string[] | undefined
+) => void;
+
+const customCompletion = (
+  current: string,
+  argv: yargs.Argv,
+  completionFilter: (onCompleted?: CompletionCallback) => any,
+  done: (completions: string[]) => any
+) => {
+  completionFilter((err: any, defaultCompletions: any) => {
+    if (!defaultCompletions) {
+      done(defaultCompletions);
+      return;
+    }
+
+    const filteredCompletions: string[] = [];
+    const aliasPattern = /^--?[a-zA-Z\d]:$/;
+    defaultCompletions.forEach((completion: string) => {
+      // TODO: remove this workaround after https://github.com/yargs/yargs/issues/2268 is fixed.
+      if (aliasPattern.test(completion)) {
+        return;
+      }
+
+      // TODO: remove this workaround after https://github.com/yargs/yargs/issues/2270 is fixed.
+      filteredCompletions.push(completion.replace(/(\r\n|\n|\r)/gm, " "));
+    });
+    done(filteredCompletions);
+  });
+};
+
 // eslint-disable-next-line no-unused-expressions
 yargs
   .command(recordCommand)
   .demandCommand()
   .strict()
   .help()
-  .completion('completion', customCompletion as any).argv;
+  .completion("completion", customCompletion as any).argv;

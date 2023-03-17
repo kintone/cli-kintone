bec7f47e1f2e67010bbb874b6cbc18c89f33aef3
Masaharu TASHIRO <masatsr.kit@gmail.com>
2023-03-17 22:12:54 +0900

    chore: remove workaround of yargs/yargs#2268 and yargs/yargs#2270

--- src/cli/main.ts
+++ src/cli/main.ts
@@ -3,33 +3,6 @@ import yargs from "yargs";
 import { recordCommand } from "./record";
 import packageJson from "../../package.json";
 
-const customCompletion = (
-  current: string,
-  argv: yargs.Argv,
-  completionFilter: (onCompleted?: CompletionCallback) => any,
-  done: (completions: string[]) => any
-) => {
-  completionFilter((err, defaultCompletions) => {
-    if (!defaultCompletions) {
-      done([]);
-      return;
-    }
-
-    const filteredCompletions: string[] = [];
-    const aliasPattern = /^--?[a-zA-Z\d]:$/;
-    defaultCompletions.forEach((completion) => {
-      // TODO: remove this workaround after https://github.com/yargs/yargs/issues/2268 is fixed.
-      if (aliasPattern.test(completion)) {
-        return;
-      }
-
-      // TODO: remove this workaround after https://github.com/yargs/yargs/issues/2270 is fixed.
-      filteredCompletions.push(completion.replace(/(\r\n|\n|\r)/gm, " "));
-    });
-    done(filteredCompletions);
-  });
-};
-
 // eslint-disable-next-line no-unused-expressions
 yargs
   .command(recordCommand)
@@ -37,4 +10,4 @@ yargs
   .strict()
   .version(packageJson.version)
   .help()
-  .completion("completion", customCompletion as any).argv;
+  .completion().argv;

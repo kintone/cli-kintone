b9be0c5e00cf990fae5aab55860bc030125a64a8
ryounasso <ryou.nasso.dev@gmail.com>
2023-06-01 09:51:57 +0900

    test: add testing of changes

---
+++ src/record/export/utils/__tests__/index.test.ts
@@ -0,0 +1,10 @@
+import { sanitizeFilename } from "../operateFilename";
+
+describe("utils/operateFilename.ts", () => {
+  it("should replace a special character", () => {
+    const testFilename = ':"t/e\\|s?*t<>.txt';
+    const expectedFilename = "__t_e__s__t__.txt";
+    const normalizedFilename = sanitizeFilename(testFilename);
+    expect(normalizedFilename).toBe(expectedFilename);
+  });
+});

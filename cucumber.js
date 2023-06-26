f4418b7c51953ffa3fc4e4f7251e380a09f1d5c3
tasshi / Masaharu TASHIRO <33759872+mshrtsr@users.noreply.github.com>
2023-06-26 16:03:14 +0900

    test: configure E2E tests (#380)
    Co-authored-by: hung-nguyen <hung-nguyen@cybozu.vn>
    Co-authored-by: ueokande <shinya-ueoka@cybozu.co.jp>

---
+++ cucumber.js
@@ -0,0 +1,10 @@
+// https://github.com/cucumber/cucumber-js/blob/main/docs/configuration.md
+const config = {
+  default: {
+    formatOptions: { snippetInterface: "synchronous" },
+    requireModule: ["ts-node/register"],
+    require: ["features/step_definitions/**/*.ts"],
+    publishQuiet: true,
+  },
+};
+module.exports = config;

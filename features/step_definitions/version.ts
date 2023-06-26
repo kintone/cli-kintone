ddbd94213cd332f1487baf2bceafe573ce994dae
Masaharu TASHIRO <masatsr.kit@gmail.com>
2023-06-26 15:55:03 +0900

    move cli-kintone execution step to common.ts

--- features/step_definitions/version.ts
+++ features/step_definitions/version.ts
@@ -1,10 +1,5 @@
 import * as assert from "assert";
-import { When, Then } from "@cucumber/cucumber";
-import { execCliKintoneSync } from "../ultils/helper";
-
-When("I run the command with args {string}", function (args: string) {
-  this.response = execCliKintoneSync(args);
-});
+import { Then } from "@cucumber/cucumber";
 
 Then(
   "I should get the version formatted in {string}",

b2e4e406d05f9b5e40306c23922e217c7dbbc8e3
hung-nguyen <hung-nguyen@cybozu.vn>
2023-08-17 14:41:25 +0700

    test: update test case

--- src/utils/__tests__/encoding.test.ts
+++ src/utils/__tests__/encoding.test.ts
@@ -3,8 +3,12 @@ import path from "path";
 
 describe("isMismatchEncoding", () => {
   it("should detect the mismatch encoding correctly", async () => {
-    const inputFilePath = path.join(__dirname, "./fixtures/input_sjis.csv");
-    expect(await isMismatchEncoding(inputFilePath, "sjis")).toBe(false);
-    expect(await isMismatchEncoding(inputFilePath, "utf8")).toBe(true);
+    const inputSJISFile = path.join(__dirname, "./fixtures/input_sjis.csv");
+    expect(await isMismatchEncoding(inputSJISFile, "sjis")).toBe(false);
+    expect(await isMismatchEncoding(inputSJISFile, "utf8")).toBe(true);
+
+    const inputUTF8File = path.join(__dirname, "./fixtures/input_utf8.csv");
+    expect(await isMismatchEncoding(inputUTF8File, "sjis")).toBe(true);
+    expect(await isMismatchEncoding(inputUTF8File, "utf8")).toBe(false);
   });
 });

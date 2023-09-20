0c37ff7e064b7a61b5eb20f5ead9102080d37ee6
Tuan Pham <tuan-pham@cybozu.vn>
2023-09-20 11:03:05 +0700

    feat: update unit test

--- src/utils/__tests__/log.test.ts
+++ src/utils/__tests__/log.test.ts
@@ -1,30 +1,53 @@
-import type { LogConfigLevel, Logger, Printer } from "../log";
-import { logger, StandardLogger } from "../log";
+import type { LogConfigLevel, LogEventLevel, Printer } from "../log";
+import { StandardLogger } from "../log";
 
-describe("logger", () => {
+describe("StandardLogger", () => {
   const mockDate = new Date(0);
   const spy = jest.spyOn(global, "Date").mockImplementation(() => mockDate);
+  const patternTest = [
+    ["DEBUG", "debug"],
+    ["INFO", "info"],
+    ["WARN", "warn"],
+    ["ERROR", "error"],
+    ["FATAL", "fatal"],
+  ];
 
-  it('should display correct log message when calling logger with "debug" level', () => {
+  it.each(patternTest)(
+    'should display %s message when calling logger with "%s" level',
+    (logDisplay, logLevel) => {
+      const message = "This is example message";
+
+      const options: { logConfigLevel: LogConfigLevel; printer: Printer } = {
+        logConfigLevel: logLevel as LogConfigLevel,
+        printer: jest.fn(),
+      };
+      const standardLogger = new StandardLogger(options);
+      standardLogger[logLevel as LogEventLevel](message);
+
+      const expectedMessage = new RegExp(
+        `\\[${mockDate.toISOString()}] (.*)${logDisplay}(.*): ${message}`,
+      );
+
+      expect(options.printer).toHaveBeenCalledWith(
+        expect.stringMatching(expectedMessage),
+      );
+    },
+  );
+
+  it("should not display any message when calling logger with 'none' level", () => {
     const message = "This is example message";
 
     const options: { logConfigLevel: LogConfigLevel; printer: Printer } = {
-      logConfigLevel: "debug",
+      logConfigLevel: "none",
       printer: jest.fn(),
     };
     const standardLogger = new StandardLogger(options);
-    standardLogger.debug(message);
+    standardLogger.info(message);
 
-    const expectedMessage = new RegExp(
-      `\\[${mockDate.toISOString()}] (.*)DEBUG(.*): ${message}`,
-    );
-
-    expect(options.printer).toHaveBeenCalledWith(
-      expect.stringMatching(expectedMessage),
-    );
+    expect(options.printer).not.toHaveBeenCalled();
   });
 
-  it("should display correct log message with multiple line message", () => {
+  it("should display the correct log message with multiple lines message", () => {
     const firstLineMessage = `This is first line message`;
     const secondLineMessage = `This is second line message`;
 
@@ -44,22 +67,24 @@ describe("logger", () => {
     );
   });
 
-  it("should display correct log message corresponding log config level", () => {
+  it("should display the correct log message corresponding to the log config level", () => {
     const message = "This is example message";
-    const options: { logConfigLevel: LogConfigLevel; printer: Printer } = {
-      logConfigLevel: "info",
-      printer: jest.fn((text: string) => {
-        return true;
-      }),
+    const options: { logConfigLevel?: LogConfigLevel; printer: Printer } = {
+      printer: jest.fn(),
     };
-
     const standardLogger = new StandardLogger(options);
+    const formatSpy = jest.spyOn(standardLogger as any, "format");
     standardLogger.setLogConfigLevel("warn");
     standardLogger.debug(message);
     standardLogger.info(message);
     standardLogger.warn(message);
     standardLogger.error(message);
     standardLogger.fatal(message);
+
+    expect(formatSpy).toHaveBeenCalledTimes(3);
+    expect(formatSpy.mock.calls[0][0]).toEqual({ level: "warn", message });
+    expect(formatSpy.mock.calls[1][0]).toEqual({ level: "error", message });
+    expect(formatSpy.mock.calls[2][0]).toEqual({ level: "fatal", message });
   });
 
   afterAll(() => {

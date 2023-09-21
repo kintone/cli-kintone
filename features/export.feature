661d148e5cc270e3303b624ec563d39bf8ea34d9
Tuan Pham <tuan-pham@cybozu.vn>
2023-09-21 12:08:02 +0700

    test: set up and tear down for e2e testing

--- features/export.feature
+++ features/export.feature
@@ -1,7 +1,8 @@
 Feature: cli-kintone export command
 
   Scenario: CliKintoneTest-81 Should return the record contents in CSV format
-    Given The app "$$TEST_KINTONE_APP_ID_FOR_EXPORT" has some records as below:
+    Given The app "$$TEST_KINTONE_APP_ID_FOR_EXPORT" with "$$TEST_KINTONE_API_TOKEN_FOR_DELETE" has no records
+    And The app "$$TEST_KINTONE_APP_ID_FOR_EXPORT" has some records as below:
       | Text   | Number |
       | Alice  | 10     |
       | Bob    | 20     |
--- features/step_definitions/setup.ts
+++ features/step_definitions/setup.ts
@@ -1,22 +1,4 @@
 import { setWorldConstructor } from "@cucumber/cucumber";
-import { Given, OurWorld } from "../ultils/world";
-import {
-  execCliKintoneSync,
-  replaceTokenWithEnvVars,
-  createCsvFile,
-} from "../ultils/helper";
+import { OurWorld } from "../ultils/world";
 
 setWorldConstructor(OurWorld);
-
-Given(
-  "The app {string} has some records as below:",
-  async function (appId, table) {
-    const tempFilePath = await createCsvFile(table.raw());
-    const command = `record import --file-path ${tempFilePath} --app ${appId} --base-url $$TEST_KINTONE_BASE_URL --username $$TEST_KINTONE_USERNAME --password $$TEST_KINTONE_PASSWORD`;
-
-    const response = execCliKintoneSync(replaceTokenWithEnvVars(command));
-    if (response.status !== 0) {
-      throw new Error(`Importing CSV failed. Error: \n${response.stderr}`);
-    }
-  },
-);

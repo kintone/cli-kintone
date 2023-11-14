8ee4244fda2b4665df8d05c343c65b9a7043609b
Nguyen Thai Hung <59815499+hung-cybo@users.noreply.github.com>
2023-11-14 15:50:21 +0900

    test: refactor replacement token (#568)

--- features/ultils/helper.ts
+++ features/ultils/helper.ts
@@ -8,10 +8,9 @@ export const execCliKintoneSync = (
 ) => {
   const response = spawnSync(
     getCliKintoneBinary(),
-    replaceTokenWithEnvVars(args).split(/\s+/),
+    replaceTokenWithEnvVars(args, options?.env).split(/\s+/),
     {
       encoding: "utf-8",
-      shell: true,
       env: options?.env ?? {},
       cwd: options?.cwd ?? process.cwd(),
     },
@@ -36,16 +35,36 @@ export const getCliKintoneBinary = (): string => {
   }
 };
 
-export const replaceTokenWithEnvVars = (input: string) =>
-  input.replace(/\$\$[a-zA-Z0-9_]+/g, replacer);
+const replaceTokenWithEnvVars = (
+  input: string,
+  envVars: { [key: string]: string } | undefined,
+) =>
+  input
+    .replace(/\$\$[a-zA-Z0-9_]+/g, processEnvReplacer)
+    .replace(/\$[a-zA-Z0-9_]+/g, inputEnvReplacer(envVars));
 
-const replacer = (substring: string) => {
+const processEnvReplacer = (substring: string) => {
   const key = substring.replace("$$", "");
   const value = process.env[key];
   if (value === undefined) {
-    throw new Error(`The env variable is missing: ${key}`);
+    throw new Error(`The env variable in process.env is missing: ${key}`);
   }
   return value;
+};
+
+const inputEnvReplacer = (envVars: { [key: string]: string } | undefined) => {
+  return (substring: string) => {
+    if (!envVars) {
+      return substring;
+    }
+
+    const key = substring.replace("$", "");
+    const value = envVars[key];
+    if (value === undefined) {
+      throw new Error(`The env variable in input parameter is missing: ${key}`);
+    }
+    return value;
+  };
 };
 
 export const createCsvFile = async (

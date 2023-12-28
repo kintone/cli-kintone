33f41c0e9b19e4c993b99e67014483728b7ac228
hung-nguyen <hung-nguyen@cybozu.vn>
2023-12-28 15:55:29 +0700

    test: add unittest

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 /** @type {import('@jest/types').Config.InitialOptions} */
 const config = {
-  roots: ["<rootDir>/src"],
+  roots: ["<rootDir>/src", "<rootDir>/features"],
   clearMocks: true,
   testPathIgnorePatterns: ["<rootDir>/node_modules/", "fixtures"],
 };

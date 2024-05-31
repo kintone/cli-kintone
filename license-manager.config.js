a4b0547435ab47be9dbb665839ed96a47c5b9c56
Masaharu Tashiro <tasshi.me@gmail.com>
2024-06-01 00:28:31 +0900

    chore: fix license issues

--- license-manager.config.js
+++ license-manager.config.js
@@ -17,14 +17,21 @@ const config = {
       "CC-BY-4.0",
       "(MIT OR CC0-1.0)",
       "(MIT OR WTFPL)",
+      "(WTFPL OR MIT)",
       "BlueOak-1.0.0",
       "(BSD-2-Clause OR MIT OR Apache-2.0)",
       "Unlicense",
     ],
-    allowPackages: ["map-stream", "pause-stream"],
+    allowPackages: ["map-stream", "pause-stream", "node-forge"],
   },
   extract: {
     output: "./NOTICE",
+  },
+  overrideLicense: (dep) => {
+    // https://github.com/felixge/node-require-like/blob/master/License
+    if (dep.name === "require-like") {
+      return "MIT";
+    }
   },
   overrideLicenseText: (dep) => {
     for (const packageName of Object.keys(OVERRIDE_LICENSES_TEXT)) {

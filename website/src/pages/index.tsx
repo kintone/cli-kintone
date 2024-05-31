2db8534b76702c1ee5afde1180e964aee7d48bee
Masaharu TASHIRO <masatsr.kit@gmail.com>
2024-05-31 19:34:56 +0900

    chore: update toppage link

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -21,7 +21,7 @@ const HomepageHeader = () => {
             className="button button--secondary button--lg"
             to="/docs/intro"
           >
-            Docusaurus Tutorial - 5min ⏱️
+            Get Started!
           </Link>
         </div>
       </div>

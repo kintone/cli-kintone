c78ac0890cfe9caf67c0d911d3a7b374d23d9a5b
Masaharu TASHIRO <masatsr.kit@gmail.com>
2024-05-31 18:50:57 +0900

    chore: prefer device color schema

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -59,6 +59,11 @@ const config: Config = {
   themeConfig: {
     // Replace with your project's social card
     image: "img/docusaurus-social-card.jpg",
+    colorMode: {
+      defaultMode: "dark",
+      disableSwitch: false,
+      respectPrefersColorScheme: true,
+    },
     navbar: {
       title: "cli-kintone",
       logo: {

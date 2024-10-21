f5decd58fb8adcde778a65ce4921b7015bc5d59b
nameless-mc <shunsuke-taniguchi@cybozu.co.jp>
2024-10-21 15:46:25 +0900

    chore: allowModules to be a variable

--- website/eslint.config.mjs
+++ website/eslint.config.mjs
@@ -2,6 +2,17 @@ import path from "node:path";
 import { fileURLToPath } from "node:url";
 import { FlatCompat } from "@eslint/eslintrc";
 
+// https://stackoverflow.com/a/74185754
+const docusaurusModuleAliases = [
+  "@docusaurus/Link",
+  "@docusaurus/useDocusaurusContext",
+  "@theme/Heading",
+  "@theme/Layout",
+  "@theme/ThemedImage",
+  "@site/static",
+  "@site/src",
+];
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
@@ -16,30 +27,14 @@ export default [
       "n/no-missing-import": [
         "error",
         {
-          allowModules: [
-            "@docusaurus/Link",
-            "@docusaurus/useDocusaurusContext",
-            "@theme/Heading",
-            "@theme/Layout",
-            "@theme/ThemedImage",
-            "@site/static",
-            "@site/src",
-          ],
+          allowModules: docusaurusModuleAliases,
         },
       ],
 
       "n/no-missing-require": [
         "error",
         {
-          allowModules: [
-            "@docusaurus/Link",
-            "@docusaurus/useDocusaurusContext",
-            "@theme/Heading",
-            "@theme/Layout",
-            "@theme/ThemedImage",
-            "@site/static",
-            "@site/src",
-          ],
+          allowModules: docusaurusModuleAliases,
         },
       ],
     },
